046b67e2b8d5f0298aa2d3865fda7a8c725a2e45
Spacefox95 <nathan.raynal@gmail.com>
2023-11-22 07:59:57 -0800

    FinalPrintf

--- printf.c
+++ printf.c
@@ -1,27 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <unistd.h>
 #include "main.h"
+/**
+ * print_format - print the correct type of variable
+ * @specifier: to compare
+ * @arg: list of variables to print
+ * Return: the number of character printed.
+ */
+int print_format(char specifier, va_list arg)
+{
+	struct sp_t spec[] = {
+		{'c', print_character},
+		{'s', print_string},
+		{'%', print_percent},
+		{'\0', NULL}
+	};
+	int i = 0;
 
+	while (spec[i].c != '\0')
+	{
+		if (specifier == spec[i].c)
+		{
+			spec[i].f(arg);
+			break;
+		}
+		i++;
+	}
+	if (spec[i].c == '\0')
+	{
+		_putchar('%');
+		_putchar(specifier);
+	}
+	return (i);
+}
 /**
  * _printf - custom printf
  * @format : the string character to print
  * Return: the number of character printed
  */
-
 int _printf(const char *format, ...)
 {
 	va_list arg;
 	int j = 0;
 	int i;
-
-	struct sp_t spec[] = {
-		{'c', print_char},
-		{'s', print_string},
-		{'%', print_percent},
-		{'\0', NULL}
-	};
 
 	va_start(arg, format);
 
@@ -31,25 +50,17 @@ int _printf(const char *format, ...)
 		{
 			i = 0;
 			format++;
-			while (spec[i].c != '\0')
+			if (*format != '\0')
 			{
-				if (*format == spec[i].c)
-				{
-					spec[i].f(arg);
-					break;
-				}
-				i++;
-			}
-			if (spec[i].c == '\0')
-			{
-				_putchar('%');
-				_putchar(*format);
+				i = print_format(*format, arg);
 			}
 		}
 		else
+		{
 			_putchar(*format);
+		}
 		format++;
-		j++;
+		j += i;
 	}
 	va_end(arg);
 	return (j);
